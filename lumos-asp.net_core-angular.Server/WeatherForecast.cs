3d027fdde1bfc31bd0dfc05a05c039309d08f504
Marcos Andrade <marcostfandrade@gmail.com>
2024-11-22 23:21:14 -0300

    Add project files.

---
+++ lumos-asp.net_core-angular.Server/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace lumos_asp.net_core_angular.Server
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
